48f9ecd93ae24619993b4e6e91a15711937295cd
arkannsk <arkannsk@mail.ru>
2018-03-20 12:57:11 +0700

    add gitignore

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
 <?
-require_once('/config/config.default.php');
-require_once(ROOT.'/components/Autoload.php');
-require_once (ROOT.'/components/Twig/Autoloader.php');
-
-$router = new Router();
-$router->run();
-
+echo 'hi';
